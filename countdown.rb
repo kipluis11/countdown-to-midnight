04da4bbad7997b0d85ce2df23c5c7744a7d99964
kip_parcell <kipluis@gmail.com>
2020-10-22 17:43:01 -0700

    commit tree a7b5e0daec7c94664903c06223012ddba31b0f3b

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
 
-def countdown
+
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+
+# countdown_with_sleep needs to take 5 seconds to execute.
+# how to to that? ... sleep("number value")
+
+def countdown_with_sleep(number)
+    while number > 0
+        puts "#{number} SECOND(S)"
+        number -= 1
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
 end
